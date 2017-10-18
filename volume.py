e12175f0671451f7bd9e1012bac5c4f3f5ca826e
s203 <s203@AI241LAB0203M13.local>
2017-10-18 11:51:42 -0700

    updates

---
+++ volume.py
@@ -0,0 +1,6 @@
+print('Welcome to the Volume Converter!')
+print('Please tell me the volume in gallons, and I will let you know how many liters you have.')
+print('How many gallons do you have?')
+gallons = float(input())
+liters = gallons * 3.7841
+print('You have ' + str(liters) + ' liters.')
